99d8e9758ca1990ea2d2d31e3303a3a05c56a01e
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2021-11-19 22:36:46 +0900

    오델로
    오델로 게임 구현.
    게임 진행시 필요한 내용들(Text) 오른쪽 화면에 띄우기 구현

--- Othello.py
+++ Othello.py
@@ -12,10 +12,10 @@ BLACK = (0,0,0)
 GREEN = (0, 128, 0)
 WHITE = (255, 255, 255)
 BLUE = (0,0,255)
+RED = (255,0,0)
+YELLOW = (255,204,51)
 
-
-
-screen = pygame.display.set_mode((400,400), 0,32)
+screen = pygame.display.set_mode((600,400), 0,32)
 pygame.display.set_caption("Othello")
 
 
@@ -30,12 +30,14 @@ for x in range(0, 8):
         pygame.draw.line(screen, BLACK, [0,x*50],[400,x*50],5)
 
 #세로 줄 긋기
-for y in range(0,8):
+for y in range(0,9):
     if y==0:
         continue
     else:    
         pygame.draw.line(screen, BLACK, [y*50,0],[y*50,400],5)
 
+#오른쪽에 상태창 만들기
+pygame.draw.rect(screen, WHITE, [403,0,200,400])
 
 
 #각 위치에서의 블럭값들 초기화
@@ -49,16 +51,14 @@ for y in range(0,SCREEN):
     screenArr.append(colList)
 
 
-        
-screenArr[3][3]=1
-screenArr[3][4]=2
-screenArr[4][3]=2
-screenArr[4][4]=1
-
-
+screenArr[3][3]=2
+screenArr[3][4]=1
+screenArr[4][3]=1
+screenArr[4][4]=2
 
 #변수
 currentTurn =1 #현재 턴- 플레이어 :1 컴퓨터 :2
+
 
 
 diagnoalScreenArr =[] #대각선 검사를 위한 변수, 3차원 배열
@@ -198,121 +198,117 @@ def setDiagonalCnt(): #대각선 검사를 위해 미리 대각선 개수 설정
 ##    print(diagnoalScreenArr[0][x])
 
 
-def InspectIfItCanBePlacedInPlace(pArrx, pArry, changeValue): #해당 위치에 블럭을 놓을 수 있는 자리인지 검사
+def InspectIfItCanBePlacedInPlace(pArrx, pArry, changeValue, pCurrentTurn): #해당 위치에 블럭을 놓을 수 있는 자리인지 검사
     returnValue=0
-
-    print("현재 턴 :", currentTurn)
     
     if 1==checkIfTherisBlock(screenArr[pArry][pArrx]):
         return 0
-
-    #print("현재 위치에는 아무것도 놓여있지 않다")
 
     #대각선 검사
     for diagonalValue in range(0,4):
         if diagnoalScreenArr[diagonalValue][pArry][pArrx] != 0:
             
             if diagonalValue==0: #왼쪽 위방향
-                if screenArr[pArry-1][pArrx-1] == changeTurn(currentTurn):
+                if screenArr[pArry-1][pArrx-1] == changeTurn(pCurrentTurn):
                     for a in range(1, diagnoalScreenArr[diagonalValue][pArry][pArrx]+1):
                         if screenArr[pArry-a][pArrx-a]==0:
                             break
-                        elif screenArr[pArry-a][pArrx-a] ==currentTurn:
+                        elif screenArr[pArry-a][pArrx-a] ==pCurrentTurn:                        
                             for b in range(1, a):
                                 if changeValue ==True:
-                                    print("왼쪽 위 방향 변경")
-                                    screenArr[pArry-b][pArrx-b] =currentTurn
-                                returnValue =1
+                                    screenArr[pArry-b][pArrx-b] =pCurrentTurn
+                            returnValue =1
+                            break
 
             if diagonalValue ==1: #오른쪽 위 방향
-                if screenArr[pArry-1][pArrx+1] == changeTurn(currentTurn):
+                if screenArr[pArry-1][pArrx+1] == changeTurn(pCurrentTurn):
                     for a in range(1, diagnoalScreenArr[diagonalValue][pArry][pArrx]+1):
                         if screenArr[pArry-a][pArrx+a]==0:
                             break
-                        elif screenArr[pArry-a][pArrx+a]==currentTurn:
+                        elif screenArr[pArry-a][pArrx+a]==pCurrentTurn:                        
                             for b in range(1, a):
                                 if changeValue ==True:
-                                    print("오른쪽 위 방향 변경")
-                                    screenArr[pArry-b][pArrx+b]=currentTurn
-                                returnValue =1
+                                    screenArr[pArry-b][pArrx+b]=pCurrentTurn
+                            returnValue =1
+                            break
 
             if diagonalValue ==2: #왼쪽 아래 방향
-                if screenArr[pArry+1][pArrx-1] == changeTurn(currentTurn):
+                if screenArr[pArry+1][pArrx-1] == changeTurn(pCurrentTurn):
                     for a in range(1, diagnoalScreenArr[diagonalValue][pArry][pArrx]+1):
                         if screenArr[pArry+a][pArrx-a]==0:
                             break
-                        elif screenArr[pArry+a][pArrx-a]==currentTurn:
+                        elif screenArr[pArry+a][pArrx-a]==pCurrentTurn:                         
                             for b in range(1, a):
                                 if changeValue ==True:
-                                    print("왼쪽 아래 방향 변경")
-                                    screenArr[pArry+b][pArrx-b]=currentTurn
-                                returnValue =1
+                                    screenArr[pArry+b][pArrx-b]=pCurrentTurn
+                            returnValue =1
+                            break
 
             if diagonalValue ==3: #오른쪽 아래 방향
-                if screenArr[pArry+1][pArrx+1] == changeTurn(currentTurn):
+                if screenArr[pArry+1][pArrx+1] == changeTurn(pCurrentTurn):
                     for a in range(1, diagnoalScreenArr[diagonalValue][pArry][pArrx]+1):
                         if screenArr[pArry+a][pArrx+a]==0:
                             break
-                        elif screenArr[pArry+a][pArrx+a]==currentTurn:
+                        elif screenArr[pArry+a][pArrx+a]==pCurrentTurn:                        
                             for b in range(1, a):
                                 if changeValue ==True:
-                                    print("오른쪽 아래 방향 변경")
-                                    screenArr[pArry+b][pArrx+b]=currentTurn
-                                returnValue =1
+                                    screenArr[pArry+b][pArrx+b]=pCurrentTurn
+                            returnValue =1
+                            break
                         
     #행 검사 - 위 방향으로 검사
     if pArry != 0: #pArry가 0이면 검사할 때 리스트 인덱스 넘어감
-        if screenArr[pArry-1][pArrx] == changeTurn(currentTurn):
+        if screenArr[pArry-1][pArrx] == changeTurn(pCurrentTurn):
             for a in range(pArry-1, -1, -1):
                 if screenArr[a][pArrx] ==0:
                     break
-                elif screenArr[a][pArrx] ==currentTurn:
+                elif screenArr[a][pArrx] ==pCurrentTurn:
                     for b in range(pArry-1, a,-1):
                         if changeValue ==True:
-                            print("행  위 방향 변경")
-                            screenArr[b][pArrx] =currentTurn
-                        returnValue =1
+                            screenArr[b][pArrx] =pCurrentTurn
+                    returnValue =1
+                    break
                         
     #행 검사 - 아래  방향으로 검사
     if pArry != SCREEN-1:
-        if screenArr[pArry+1][pArrx] == changeTurn(currentTurn):
+        if screenArr[pArry+1][pArrx] == changeTurn(pCurrentTurn):
             for a in range(pArry+1, SCREEN):
                 if screenArr[a][pArrx] ==0:
                     break
-                elif screenArr[a][pArrx]==currentTurn:
+                elif screenArr[a][pArrx]==pCurrentTurn:
                     for b in range(pArry+1, a):
                         if changeValue ==True:
-                            print("행 아래 방향 변경")
-                            screenArr[b][pArrx]=currentTurn
-                        returnValue =1
+                            screenArr[b][pArrx]=pCurrentTurn
+                    returnValue =1
+                    break
                     
                         
     #열 검사 - 왼쪽 방향으로 검사
     if pArrx !=0:
-        if screenArr[pArry][pArrx-1] == changeTurn(currentTurn):
+        if screenArr[pArry][pArrx-1] == changeTurn(pCurrentTurn):
             for a in range(pArrx-1, -1,-1):
                 if screenArr[pArry][a] ==0:
                     break
-                elif screenArr[pArry][a] ==currentTurn:
+                elif screenArr[pArry][a] ==pCurrentTurn:
                     for b in range(pArrx-1, a, -1):
                         if changeValue ==True:
-                            print("열 왼쪽 방향 변경")
-                            screenArr[pArry][b] =currentTurn
-                        returnValue =1
+                            screenArr[pArry][b] =pCurrentTurn
+                    returnValue =1
+                    break
 
                         
     #열 검사 - 오른쪽 방향으로 검사
     if pArrx != SCREEN-1:
-        if screenArr[pArry][pArrx+1] == changeTurn(currentTurn):
+        if screenArr[pArry][pArrx+1] == changeTurn(pCurrentTurn):
             for a in range(pArrx+1, SCREEN):
                 if screenArr[pArry][a] ==0:
                     break
-                elif screenArr[pArry][a] ==currentTurn:
+                elif screenArr[pArry][a] ==pCurrentTurn:
                     for b in range(pArrx+1, a):
                         if changeValue ==True:
-                            print("열 오른쪽 방향 변경")
-                            screenArr[pArry][b] =currentTurn
-                        returnValue =1
+                            screenArr[pArry][b] =pCurrentTurn
+                    returnValue =1
+                    break
 
                     
     return returnValue #놓을 수 있는 곳이  없을 경우:0   있을 경우 :1
@@ -331,7 +327,7 @@ def setWhereComputerCanPutBlock():
     noMeaningStorage=0
     computerRandomPlace =[]
 
-    #computerRandomPlace 모두 0으로 초기화(8x8 2차원 배)
+    #computerRandomPlace 모두 0으로 초기화(8x8 2차원 배열)
     for y in range(0,SCREEN):
         colList =[]
         for x in range(0,SCREEN):
@@ -341,15 +337,11 @@ def setWhereComputerCanPutBlock():
     
     for row in range(0, SCREEN):
         for col in range(0,SCREEN):
-            if InspectIfItCanBePlacedInPlace(col, row, False) ==1:
+            if InspectIfItCanBePlacedInPlace(col, row, False, currentTurn) ==1:
                 computerRandomPlace[row][col] = randomComputerNum
                 randomComputerNum = randomComputerNum +1
-        
-        print(computerRandomPlace[row])
-        
-    randomComputerNum = calculateComputerRandomPlace(randomComputerNum)
-    print()
-    print()
+                       
+    randomComputerNum = calculateComputerRandomPlace(randomComputerNum-1) #-1하는 이유 맨 마지막에 +1돼서 끝나기 때문
     
     for row in range(0,SCREEN):
         for col in range(0,SCREEN):
@@ -358,25 +350,211 @@ def setWhereComputerCanPutBlock():
                 tmpRow = row
                 tmpCol = col
 
-    #컴퓨터가 랜덤으로 놓을 위치 미리 보여주기
-
-  
-    
+    #컴퓨터가 랜덤으로 놓을 위치 미리 보여주기  
     viewGameScreen()
     pygame.display.update()
     
     #해당 위치 원래 컴퓨터 블럭색으로 변경
     time.sleep(2)
-    noMeaningStorage = InspectIfItCanBePlacedInPlace(tmpCol, tmpRow, True)
+    noMeaningStorage = InspectIfItCanBePlacedInPlace(tmpCol, tmpRow, True, currentTurn)
     screenArr[tmpRow][tmpCol] = 2 #컴퓨터가 랜덤으로 놓을위치 원래색인 하얀색으로 변경
     
     viewGameScreen()
     pygame.display.update()
 
-    
 
+def moveNextTurn(): #둘 곳이 없을 경우 다음턴으로 넘어간다.
+    global currentTurn
+    global isClick
+    cannotPutPlaceCnt =0
+    
+    for row in range(0,SCREEN):
+        for col in range(0,SCREEN):
+            if screenArr[row][col] == 0:
+                if InspectIfItCanBePlacedInPlace(col,row,False, currentTurn)==1:
+                    cannotPutPlaceCnt = cannotPutPlaceCnt+1
+                    
+    if cannotPutPlaceCnt ==0 :
+        currentTurn = changeTurn(currentTurn)
+        print(currentTurn,"의 유저가 놓을 곳이 없습니다. ")
+        clearStateScreen(False)
+        printTurnInformation() #플레이어 -> 컴퓨터 턴 : 컴퓨터 턴 출력
+        time.sleep(1)
+
+
+def viewGameResult():
+    font = pygame.font.SysFont("arial",20,True)
+    playerBlockCnt =0
+    computerBlockCnt =0
+
+    for row in range(0,SCREEN):
+        for col in range(0,SCREEN):
+            if screenArr[row][col] ==1:
+                playerBlockCnt = playerBlockCnt+1
+            elif screenArr[row][col] ==2:
+                computerBlockCnt = computerBlockCnt+1
+                
+    tmpBlockCnt =0
+
+    for row in range(0,SCREEN):
+        for col in range(0,SCREEN):
+            if tmpBlockCnt < playerBlockCnt:
+                screenArr[row][col] =1
+                tmpBlockCnt = tmpBlockCnt+1
+            else:
+                screenArr[row][col] =2
+                
+    print("컴퓨터 블럭 개수 : ", computerBlockCnt)
+    print("플레이어 블럭 개수 : ", playerBlockCnt)
+
+    if computerBlockCnt < playerBlockCnt:
+        print("플레이어 승리")
+    elif computerBlockCnt > playerBlockCnt:
+        print("컴퓨터 승리")
+    else: #동점
+        print("무승부")
+
+    clearStateScreen(True)
+    
+    if computerBlockCnt < playerBlockCnt:
+        printWinner("Player")
+    elif computerBlockCnt > playerBlockCnt:
+        printWinner("Computer")
+    else: #동점
+        printWinner("Draw")
+    
+    viewGameScreen()
+    printBlockCnt(playerBlockCnt, computerBlockCnt)
+    pygame.display.update()
+    print("개수 출력화면까지 끝")
+    time.sleep(3)
+    sys.exit()
                 
 
+def ifNoOneDoNotPutBlock():
+    enablePutBlock= [True,True]
+    
+    for row in range(0,SCREEN):
+        for col in range(0,SCREEN):
+            #플레이어 검사와 검퓨터 모두 블럭을 둘 곳이 없을 경우
+            if 1==InspectIfItCanBePlacedInPlace(col,row,False,1):
+                #print("플레이어 : (",row,col,") : 0")
+                enablePutBlock[0] = False
+            if 1==InspectIfItCanBePlacedInPlace(col,row,False,2):
+                #print("컴퓨터   : (",row,col,") : 0")
+                enablePutBlock[1] = False
+                
+
+    if enablePutBlock[0] ==True and enablePutBlock[1] ==True:
+        return True
+    else:
+        return False
+    
+
+def checkGameOver():
+    spaceFilledCnt =0
+    
+    for row in range(0,SCREEN):
+        for col in range(0,SCREEN):
+            if screenArr[row][col] ==1 or screenArr[row][col] ==2:
+                spaceFilledCnt= spaceFilledCnt+1
+                
+    
+    if spaceFilledCnt == SCREEN * SCREEN or ifNoOneDoNotPutBlock() == True:              
+        clearStateScreen(True)
+        printGameOverText()
+        printCalculateGameResult()
+        time.sleep(5) #결과 집계중 5초동안 띄운 뒤 결과 보여주기
+        viewGameResult()
+
+
+def printTurn(pTurn):
+    if pTurn ==1:
+        return "Player"
+    elif pTurn ==2:
+        return "Computer"
+    else:
+        return "Error"
+    
+
+def clearStateScreen(isGameOver):
+    clearScreenScaleY =80
+    
+    if isGameOver == True:
+        clearScreenScaleY = 400
+        print("sclearScreenScaleY =400")
+    
+    pygame.draw.rect(screen, WHITE, [403,0,200,clearScreenScaleY])
+    pygame.display.update()
+
+
+def printTurnInformation():
+    userTextFont = pygame.font.SysFont("arial",20, True)
+    userTextContentFont = pygame.font.SysFont("arial",20)
+    
+    userText = userTextFont.render("Current Turn : ", True, BLACK)
+    userTextContent = userTextContentFont.render(printTurn(currentTurn), True, BLACK)
+    
+    screen.blit(userText, (410,100))
+    screen.blit(userTextContent, (525,100))
+    pygame.display.update()
+
+
+def printUserColorInformation():
+    font = pygame.font.SysFont("arial",20,True)
+    playerColor = font.render("Player Color : ", True, BLACK)
+    computerColor = font.render("Computer Color : ", True, BLACK)
+    
+    screen.blit(playerColor, (410,150))
+    screen.blit(computerColor, (410,200))
+
+    pygame.draw.rect(screen, GREEN, (548, 148, 30, 30))
+    pygame.draw.circle(screen, BLACK, [563, 163], 10) 
+    pygame.draw.rect(screen, GREEN, (548, 198, 30, 30))
+    pygame.draw.circle(screen, WHITE, [563, 213], 10)
+    
+    pygame.display.update()
+
+
+def printGameOverText():
+    font = pygame.font.SysFont("arial",30,True)
+    text = font.render("-Game Over-", True, RED)
+    
+    screen.blit(text, (425,50))
+    pygame.display.update()
+
+
+def printCalculateGameResult(): #게임 결과 계산중 이라고 출력
+    font = pygame.font.SysFont("arial",15)
+    text = font.render("~Calculating Game Result~", True, BLACK)
+    
+    screen.blit(text, (425,100))
+    pygame.display.update()
+
+
+def printWinner(winner):
+    
+    winnerFont = pygame.font.SysFont("arial",40)
+    winnerContentFont = pygame.font.SysFont("arial",30)
+    
+    winnerText = winnerFont.render("Winner", True, RED)
+    winnerContentText = winnerContentFont.render("-"+winner+"-", True, YELLOW)
+    
+    screen.blit(winnerText, (450,50))
+    screen.blit(winnerContentText, (440,100))
+    pygame.display.update()
+
+
+
+def printBlockCnt(playerBlockCnt, computerBlockCnt):
+    font = pygame.font.SysFont("arial",20)
+    
+    playerBlockCntText = font.render("Player Block : "+ str(playerBlockCnt), True, BLACK)
+    computerBlockCntText = font.render("Computer Block : " + str(computerBlockCnt), True, BLACK)
+    
+    screen.blit(playerBlockCntText, (450,200))
+    screen.blit(computerBlockCntText, (440,225))
+    pygame.display.update()
 
 
 
@@ -384,47 +562,66 @@ def setWhereComputerCanPutBlock():
 
 
 
+#둘다 블럭을  놓을 수 없는 경우
+##for row in range(0,SCREEN):
+##        for col in range(0,SCREEN):
+##            screenArr[row][col] =2
+##screenArr[2][6] =1
+##screenArr[2][2] =1
+##screenArr[3][3] =1
+##screenArr[4][4] =1
+##screenArr[4][6] =1
+##screenArr[5][5] =1
+##screenArr[7][7] =1
+##screenArr[6][7] =0
 
-ComputerControl = False
+for row in range(0,SCREEN):
+        for col in range(0,SCREEN):
+            screenArr[row][col] =2
+screenArr[0][0] =1
 
 setDiagonalCnt()
 viewGameScreen()
+printTurnInformation()
+printUserColorInformation()
 
 
 #Game Loop
-while True:
+while True:   
+    checkGameOver()
+    
+    
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
             
-        if event.type == pygame.MOUSEBUTTONDOWN:           
-            print("마우스클릭")
-           
+        if event.type == pygame.MOUSEBUTTONDOWN:
             tmpMousePos = []
             tmpMousePos.append(changeMousePosXToArrx(pygame.mouse.get_pos()[0]))
             tmpMousePos.append(changeMousePosYToArry(pygame.mouse.get_pos()[1]))
-            print(currentTurn)
+    
             if not(tmpMousePos[0] ==-1 or tmpMousePos[1] ==-1):
-                if InspectIfItCanBePlacedInPlace(tmpMousePos[0],tmpMousePos[1], True) ==1: 
+                if InspectIfItCanBePlacedInPlace(tmpMousePos[0],tmpMousePos[1], True, currentTurn) ==1:
                     mousePos = pygame.mouse.get_pos() #자료형 : tuple
-                    screenArr[changeMousePosYToArry(mousePos[1])][changeMousePosXToArrx(mousePos[0])] =1 #클릭한 곳 색깔 바꾸기
-                    #print(mousePos)     
+                    screenArr[changeMousePosYToArry(mousePos[1])][changeMousePosXToArrx(mousePos[0])] =1 #클릭한 곳 색깔 바꾸기    
                     currentTurn = changeTurn(currentTurn) #턴 바꾸기
-                    controlComputerTurn = True
-                    print("****************************")
-                    time.sleep(2)
+                    clearStateScreen(False)
+                    printTurnInformation() #플레이어 -> 컴퓨터 턴 : 컴퓨터 턴 출력
+                    
+                    
+    moveNextTurn()
     
     viewGameScreen()
     pygame.display.update()
-    if currentTurn ==2 and controlComputerTurn ==True:
-        print("컴퓨터 턴 시작!!!!!!!!!!!")
+    
+    if currentTurn ==2 :
+        time.sleep(2)
         setWhereComputerCanPutBlock()
-        print("컴퓨터 턴 끝!!!!!!!!!!!!!")
-        #print(currentTurn)
         currentTurn = changeTurn(currentTurn)
-        controlComputerTurn = False
-        
+        clearStateScreen(False)
+        printTurnInformation() #컴퓨터 -> 플레이어 턴 : 컴퓨터 턴 출력
+       
 
 
 
